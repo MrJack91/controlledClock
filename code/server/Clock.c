440a8572cb118c26eaa2d8bf05a709c9a87e0592
Dani <daniel.brun@gmx.net>
2014-04-25 11:16:24 +0200

    Merge into server

--- code/server/Clock.c
+++ code/server/Clock.c
@@ -1,40 +1,239 @@
 /* 
- * File:   Clock.c
+ * File:   clock.c
  * Author: Daniel Brun
- *
- * Created on 5. April 2014, 12:26
+ * Created: 10.04.2014
+ * 
+ * Description: 
+ * Provides a clock function which relays on the system tic.
+ *  
  */
 
+/*---------------------------- Includes: System ------------------------------*/
 #include <stdio.h>
 #include <stdlib.h>
-
-#include "Clock.h"
+#include <time.h>
 #include <signal.h>
+#include <semaphore.h>
 
-void SignalHandler(int signal)
-{
-    printf("Signal: %d", signal);
-    if (signal == SIGABRT) {
-        // abort signal handler code
-    } else {
-        // ...
+/*---------------------------- Includes: User-Libs ---------------------------*/
+#include "clock.h"
+
+/*---------------------------- Declarations ----------------------------------*/
+
+#define TIMER_RESOLUTION 1 
+
+TimeStruct currentTime;
+TimeStruct lastSyncTime;
+
+int ticsPerSecond;
+int currentTics;
+
+int synched;
+
+sem_t clockSem;
+sem_t timerSem;
+sem_t syncSem;
+/*---------------------------- Internal functions ----------------------------*/
+
+void loadFromSystem();
+
+void tic();
+
+void timerHandler(int signum);
+
+/*---------------------------- Implementations -------------------------------*/
+void clock_start(){
+    if (sem_init(&clockSem, 0, 1) != 0) {
+        perror("Clock Semaphore initialization failed");
+        exit(EXIT_FAILURE);
+    }
+    
+    if (sem_init(&timerSem, 0, 1) != 0) {
+        perror("Timer Semaphore initialization failed");
+        exit(EXIT_FAILURE);
+    }
+    
+    if (sem_init(&syncSem, 0, 1) != 0) {
+        perror("Sync Semaphore initialization failed");
+        exit(EXIT_FAILURE);
+    }
+    
+    loadFromSystem();
+    
+    ticsPerSecond = 1;
+    currentTics = 0;
+    synched = 0;
+    
+    signal (SIGALRM, timerHandler);
+    alarm (TIMER_RESOLUTION);
+    
+}
+
+void clock_shutdown(){
+    sem_destroy(&clockSem);
+    sem_destroy(&timerSem);
+    sem_destroy(&syncSem);
+}
+
+TimeStruct clock_getCurrentTime(){
+    return currentTime;
+}
+
+TimeStruct clock_getLastSyncTime(){
+    return lastSyncTime;
+}
+
+void clock_syncTime(TimeStruct aTime){
+    sem_wait(&syncSem);
+    sem_wait(&timerSem);
+    sem_wait(&clockSem);
+   
+    //cancel alarm
+    alarm(0);
+    
+    //setup next alarm
+    alarm (TIMER_RESOLUTION);
+     
+    synched = 1;
+    
+    //Sync time
+    currentTics = 0;
+    currentTime = aTime;
+    
+    sem_post(&clockSem);
+    sem_post(&timerSem);
+    sem_post(&syncSem);
+}
+    
+void clock_ticSecond(){
+     //Abort if sync is in process
+    int syncSemVal;
+    sem_getvalue(&syncSem,&syncSemVal);
+    
+    if(syncSemVal == 0){
+        return;
+    }
+    
+    sem_wait(&timerSem);
+    sem_wait(&clockSem);
+    
+    //cancel alarm
+    alarm(0);
+    
+    //setup next alarm
+    alarm (TIMER_RESOLUTION);
+
+    currentTics = ticsPerSecond - 1;
+    sem_post(&clockSem);
+    sem_post(&timerSem);
+    
+    tic();
+}
+
+void timerHandler(int signum){
+    //Abort if sync is in process
+    int syncSemVal;
+    sem_getvalue(&syncSem,&syncSemVal);
+    
+    if(syncSemVal == 0){
+        return;
+    }
+    
+    sem_wait(&timerSem);
+       
+    alarm (TIMER_RESOLUTION);
+
+    tic();
+    sem_post(&timerSem);
+}
+
+void tic(){
+    sem_wait(&clockSem);
+
+    currentTics++;
+
+    //Second passed
+    if(currentTics >= ticsPerSecond){
+        currentTime.second++;
+
+        if(currentTime.second == 60){
+            currentTime.second = 0;
+            currentTime.minute++;
+
+            if(currentTime.minute == 60){
+                currentTime.minute = 0;
+                currentTime.hour++;
+
+                if(currentTime.hour == 24){
+                    currentTime.hour = 0;
+                    currentTime.day++;
+
+                    int maxDay = 31;
+                    int daysInYear = 365;
+
+                    switch(currentTime.month){
+                        case 4:
+                        case 6:
+                        case 9:
+                        case 11:
+                            maxDay = 30;
+                            break;
+                        case 2:
+                            if((((currentTime.year % 4) == 0) && !((currentTime.year % 100) == 0)) || (((currentTime.year % 4) == 0) && ((currentTime.year % 400) == 0))){
+                                maxDay = 29;
+                                daysInYear = 366;
+                            }else{
+                                maxDay = 28;
+                            }
+                            break;
+                    }
+
+                    if(currentTime.day > maxDay){
+                        currentTime.day = 1;
+                        currentTime.month++;
+
+                        if(currentTime.month > 12){
+                            currentTime.month = 1;
+                            currentTime.year++;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    sem_post(&clockSem);
+}
+
+void loadFromSystem(){
+    time_t t = time(NULL);
+    struct tm lt = *localtime(&t);
+    
+    //TODO: Use Sempaphores
+    lastSyncTime.year = (lt.tm_year + 1900);
+    lastSyncTime.month = (lt.tm_mon + 1);
+    lastSyncTime.day = lt.tm_mday;
+    lastSyncTime.hour = lt.tm_hour;
+    lastSyncTime.minute = lt.tm_min;
+    lastSyncTime.second = lt.tm_sec;
+    
+    if(lt.tm_isdst == 0){
+         lastSyncTime.zoneOffset = 1;
+    }else{
+         lastSyncTime.zoneOffset = 2;
+    }
+    
+    //TODO: Use Sempaphores
+    currentTime.year = (lt.tm_year + 1900);
+    currentTime.month = (lt.tm_mon + 1);
+    currentTime.day = lt.tm_mday;
+    currentTime.hour = lt.tm_hour;
+    currentTime.minute = lt.tm_min;
+    currentTime.second = lt.tm_sec;
+    
+    if(lt.tm_isdst == 0){
+         currentTime.zoneOffset = 1;
+    }else{
+         currentTime.zoneOffset = 2;
     }
 }
-
-void initializeClock(){
-    
-    //Perhaps the new way: http://msdn.microsoft.com/en-us/library/xdkz3x12.aspx
-    
-    
-    //http://www.mycfiles.com/2011/10/interrupt-programming-in-c.html
-    //http://cboard.cprogramming.com/c-programming/141234-need-pointer-help-interrupt-vectors.html
-    
-    typedef void (*SignalHandlerPointer)(int);
- 
-    SignalHandlerPointer previousHandler;
-    previousHandler = signal(SIGABRT, SignalHandler);
- 
-    abort();
-}
-
-// SOURCE: http://www.intel-assembler.it/portale/5/high-precision-timer-clock/source-code-fast-timer-8259-c-asm.asp
--- code/server/main.c
+++ code/server/main.c
@@ -1,103 +1,132 @@
 /* 
- * File:   main.c
- * Author: Daniel Brun
- *
- * Created on 1. April 2014, 12:59
+ * File:    main.c
+ * Author:  Daniel Brun
+ * Created: 01.04.2014
+ * 
+ * Description: 
+ * Main programm of the 'ControlledClock'. Starts and coordinates the different
+ * modules.
+ *  
  */
 
+/*---------------------------- Includes: System ------------------------------*/
 #include <stdio.h>
 #include <stdlib.h>
-#include "SimpleSocketServer.h"
-#include "Clock.h"
-#include <pthread.h>
+#include <string.h>
 
+#include <pthread.h>
 #include <signal.h>
 
+/*---------------------------- Includes: User-Libs ---------------------------*/
+#include "SimpleSocketServer.h"
+#include "helper_json.h"
+#include "clock.h"
 
-/*
+/*---------------------------- Declarations ----------------------------------*/
+int cleanUpExecuted = 0;
+
+/*---------------------------- Internal functions ----------------------------*/
+
+/**
+ * Handles the http request from the web server. A json string with the current
+ * time from the clock module und the last sync time will be sent back to the
+ * client.
  * 
+ * @param content The request content.
+ * @return The content to be sent to the client.
  */
-typedef enum {
-    false, true
-} bool;
+void server_handle(char *content,char **response);
 
-void handle(int aNumber);
-
-void InterruptSignalHandler(int signalType);
-
-void cleanUp();
+/**
+ * Shuts all modules properly down and exits.
+ */
+void main_exit();
 
 int main(int argc, char *argv[]) {
 
-    //TODO: use log4c
-
-
-    //First startup sequence
-
-    //Pass function pointer to methods...
-
-    //Start DCF77 Reader
-
-    //Start DCF77 Simulator
-
-    //Start analyzer 
-
-    //Start encoder
-
-    //Semaphores:
-    //http://www.daniweb.com/software-development/c/threads/330806/semaphores-in-c
-
-    //Function Pointers -> Callback
-    //http://www.cprogramming.com/tutorial/function-pointers.html
-
-    //http://www.chemie.fu-berlin.de/chemnet/use/info/libc/libc_21.html
-
- initializeClock();
-    //Thread exmplae
-    int serverParam = 1;
+    //Setup signal handlers for unexcpected termination
+    if (signal (SIGINT, main_exit) == SIG_IGN){
+         signal (SIGINT, SIG_IGN);
+    }
+    if (signal (SIGKILL, main_exit) == SIG_IGN){
+         signal (SIGKILL, SIG_IGN);
+    }
+    if (signal (SIGHUP, main_exit) == SIG_IGN){
+         signal (SIGHUP, SIG_IGN);
+    }
+    if (signal (SIGTERM, main_exit) == SIG_IGN){
+         signal (SIGTERM, SIG_IGN);
+    }
+    
     pthread_t serverThread;
-    pthread_t inlineThread;
-
+    pthread_t clockThread;
+    
     pthread_create(&serverThread,
             NULL,
-            (void *) runServer,
-            (void *) handle);
-
-    pthread_create(&inlineThread,
+            (void *) server_start,
+            (void *) server_handle);
+    pthread_create(&clockThread,
             NULL,
-            (void *) Hello,
-            (void *) NULL);
-
+            (void *) clock_start,
+            NULL);
+   
     pthread_join(serverThread, NULL);
-    //pthread_join(inlineThread, NULL);
-   
-    atexit(cleanUp);
+    pthread_join(clockThread, NULL);
+    
+    atexit(main_exit);
     exit(0);
-   
-    //!!!!http://www.intel-assembler.it/portale/5/high-precision-timer-clock/source-code-fast-timer-8259-c-asm.asp
-    
-    //Interrupt Timer: http://www.osdever.net/bkerndev/Docs/pit.htm
-    //http://www.inversereality.org/tutorials/interrupt%20programming/timerinterrupt.html
-    
-    //Interrupts from keyboard:
-    //http://www.fysnet.net/demoisr.htm
-    //High precision system clock: http://www.delorie.com/gnu/docs/glibc/libc_436.html
-    //Hello();
-    
-    //Assembler wrapper: http://www.techrepublic.com/article/writing-complex-interrupt-handlers-in-c/#.
+
     return (EXIT_SUCCESS);
 }
 
-void handle(int aNumber) {
+void server_handle(char *content, char **response) {
+    
+    printf("Client-Request: %s\n", content);
+    
+    TimeStruct currTime = clock_getCurrentTime();
+    TimeStruct lastSyncTime = clock_getLastSyncTime();
+    
+    char currTimeStr[26];
+    char lastSyncTimeStr[26];
+    
+    sprintf(currTimeStr,"%04d-%02d-%02dT%02d:%02d:%02d%+2.2d:00",currTime.year,currTime.month,currTime.day,currTime.hour,currTime.minute,currTime.second,currTime.zoneOffset);
+    sprintf(lastSyncTimeStr,"%04d-%02d-%02dT%02d:%02d:%02d%+2.2d:00",lastSyncTime.year,lastSyncTime.month,lastSyncTime.day,lastSyncTime.hour,lastSyncTime.minute,lastSyncTime.second,lastSyncTime.zoneOffset);
+    
+    KeyValuePair kvPairs[2];
+    kvPairs[0].key = "CurrentTime";
+    kvPairs[0].value = currTimeStr;
+    
+    kvPairs[1].key = "LastSyncTime";
+    kvPairs[1].value = lastSyncTimeStr;
+    
+    char *jsonString = json_createString(kvPairs);
+    
+    int length = strlen(jsonString) * sizeof(*jsonString);
+    *response = malloc(length);
 
-    printf("Handle was %d\n", aNumber);
+    if(*response == NULL){
+           fprintf(stderr, "Failed to allocate memory for json conversion!\n");
+           exit(2);
+    }
+    
+    strncpy(*response,jsonString,length);
+    (*response)[length] = '\0';
+    
+    free(jsonString);
+    jsonString = NULL;
+    
 }
 
-void InterruptSignalHandler (int signalType) {
-printf ("Interrupt received. Exiting program.\n");
-exit(1);
-}
+void main_exit(){
+    
+    if(cleanUpExecuted != 1){
+        cleanUpExecuted = 1;
+        printf("Shutdown application...\n");
 
-void cleanUp(){
-    printf("Clean up before shutdown....");
+        server_stop();
+
+        clock_shutdown();
+        printf("Application shutdown finished...exiting now...\n");
+    }
+    exit(1);
 }
