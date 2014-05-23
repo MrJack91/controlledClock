9b95858929c97715b38551ceb09320ae86a919c9
Michael Hadorn <michael.hadorn@gmail.com>
2014-05-23 14:48:17 +0200

    fix mergeconflict init.js

--- code/client/assets/js/init.js
+++ code/client/assets/js/init.js
@@ -1,6 +1,18 @@
 var myCoolClock;
+var timeoutSuccessNotify = 0;
 
 $(document).ready(function () {
+  // set for jquery ui swissgerman as default
+  $.datepicker.setDefaults($.datepicker.regional['de']);
+
+  // register jquery ui
+  $('#inputDate').datepicker(
+    {
+      defaultDate: '+0'
+    }
+  );
+  // set prefilled date
+  $('#inputDate').datepicker('setDate', '+0');
 
   //Enable Cross-Domain-Support
   $.support.cors = true;
@@ -17,9 +29,11 @@ $(document).ready(function () {
     logClockRev:    false
   });
 
+  myCoolClock.tickDelay = 100;
+
   // expand coolclock
   myCoolClock.expandClock();
-  myCoolClock.setTime(0);
+  myCoolClock.setTime(new Date().getTime());
 
   $('#btnSetTimeManual').on('click', setTimeManual);
   $('#btnSyncTime').on('click', fireSyncTimeDcf77);
@@ -27,9 +41,13 @@ $(document).ready(function () {
 
   // init date fields with current values
   var curDate = new Date();
-  var fullDate = curDate.getDay()+'.' + curDate.getDate() + '.' + curDate.getFullYear();
-  // $('#inputDate').val(fullDate);
-  $('#inputTime').val(curDate.getHours() + ':' + curDate.getMinutes());
+  var fullDate = pad(curDate.getDate(),2) + '' + '.' + curDate.getFullYear();
+  /*
+   $('#inputDate').attr('placeholder', fullDate);
+   */
+  var fullTime = pad(curDate.getHours(), 2) + ':' + pad(curDate.getMinutes(), 2) + ':00';
+  $('#inputTime').val(fullTime);
+  $('#inputTime').attr('placeholder', fullTime);
 });
 
 
@@ -41,6 +59,7 @@ function fireSyncTimeDcf77(e) {
 
 function syncTimeDcf77(){
   var timeSend = new Date();
+  setSyncState('run');
   $.ajax({
     url: "http://localhost:7899/",
     type: "GET",
@@ -48,7 +67,15 @@ function syncTimeDcf77(){
     success: function (response) {
       var resp = JSON.parse(response);
       var curDate = new Date(resp.CurrentTime);
+      if (isNaN(curDate.getDate())) {
+        curDate = new Date(0);
+      }
       var syncDate = new Date(resp.LastSyncTime);
+      /*
+      if (isNaN(syncDate.getDate())) {
+        syncDate = new Date(0);
+      }
+      */
       // ntp
       var receiveTime = new Date();
       var totalTransferTime = receiveTime.getTime() - timeSend.getTime();
@@ -58,30 +85,56 @@ function syncTimeDcf77(){
 
       // check the dates
       var syncDateTime = syncDate.toLocaleString();
-      console.log(syncDateTime);
-      if (syncDate !== true) {
+      if (isNaN(syncDate.getDate())) {
         syncDateTime = 'Invalid Date (' + resp.LastSyncTime + ')';
-		console.log(syncDateTime);
-		syncDateTime = '-';
+        console.log(syncDateTime);
+        syncDateTime = '-';
       }
 
       $('#spLastSyncServer').html(curDate.toLocaleString());
       $('#spLastSyncDCF77').html(syncDateTime);
+      setSyncState('success');
+      timeoutSuccessNotify = setTimeout(fadeOutSyncNotify, 800);
     },
     error: function (xhr, status) {
+      setSyncState('error');
       console.log(xhr);
       console.log(status);
     }
   });
 }
 
+function fadeOutSyncNotify() {
+  $('#spSyncSuccess').fadeOut(300);
+  timeoutSuccessNotify = setTimeout('setSyncState(\'idle\')', 300);
+}
+
 function setTimeManual(e) {
   var sDate = $('#inputDate').val();
   var sTime = $('#inputTime').val();
+  var arrDate = sDate.split('.');
+  var arrTime = sTime.split(':');
 
-  var newDate = new Date(sDate+ ' ' + sTime);
+  arrTime[2] = parseInt(arrTime[2]);
+  if (isNaN(arrTime[2])) {
+    arrTime[2] = 0;
+  }
 
-  console.log(newDate);
+  // fix month
+  var monthForDate = (parseInt(arrDate[1])+11)%12;
+  var newDate = new Date(arrDate[2], monthForDate, arrDate[0], arrTime[0], arrTime[1], arrTime[2], 0);
+  if (newDate.getFullYear() == arrDate[2]) {
+    // no errors
+    $('#fgDate').removeClass('has-error');
+    $('#fgTime').removeClass('has-error');
+    console.log(newDate + 'no error');
+  } else {
+    // error
+    console.log('there was an error');
+    $('#fgDate').addClass('has-error');
+    $('#fgTime').addClass('has-error');
+  }
+
 
   myCoolClock.setTime(newDate.getTime());
 
@@ -90,11 +143,42 @@ function setTimeManual(e) {
 }
 
 function setupAutoSync(e){
-
-	if($('#cbAutoSync').is(":checked")){
-		myCoolClock.autoTimerId = setInterval(syncTimeDcf77, 1000);
-	}else{
-		clearInterval(myCoolClock.autoTimerId);
-	}
+  if($('#cbAutoSync').is(":checked")){
+    myCoolClock.autoTimerId = setInterval(syncTimeDcf77, 1000);
+  }else{
+    clearInterval(myCoolClock.autoTimerId);
+  }
 }
 
+function setSyncState(newState) {
+  // clear timeout if there is any
+  if (timeoutSuccessNotify > 0) {
+    clearTimeout(timeoutSuccessNotify);
+    timeoutSuccessNotify = 0;
+  }
+
+  // hide all states
+  $('.labelSync').hide();
+  // show new state
+  var eleState = '';
+  switch (newState) {
+    case 'idle':
+      eleState = 'spSyncIdle';
+      break;
+    case 'run':
+      eleState = 'spSyncRun';
+      break;
+    case 'success':
+      eleState = 'spSyncSuccess';
+      break;
+    case 'error':
+      eleState = 'spSyncError';
+      break;
+  }
+  $('#'+eleState).fadeIn();
+}
+
+function pad(num, size) {
+  var s = "0000" + num;
+  return s.substr(s.length-size);
+}
